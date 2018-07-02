373fc169e3af75afbff16364bdf09b79d7f8b9fa
ivanovit <i.ivanov.it@gmail.com>
2018-07-02 13:29:31 +0300

    Bring back tslint

--- src/tests/pathTransformData.ts
+++ src/tests/pathTransformData.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:max-line-length */
 const tests = [
     { platform: 'android', scriptUrl: 'file:///data/data/org.nativescript.TabNavigation/files/app/main.js', expectedResult: 'C:\\projectpath\\app\\main.js', existingPath: 'C:\\projectpath\\app\\main.js' },
     { platform: 'android', scriptUrl: 'VM1', expectedResult: 'VM1' },
